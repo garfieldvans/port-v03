49dc6935b7efa3474fede86fd81b8b3b57282e7e
garfieldvans <iyanhalo@gmail.com>
2023-12-19 12:45:35 +0700

    work experiences 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import css from "./styles/app.module.scss";
 import About from "./components/hero/About";
 import Skills from "./components/skills/Skills";
 import Portfolio from "./components/portfolio/Portfolio";
-import Work from "./components/work/work";
+import Work from "./components/work/Works";
 
 const App = () => {
   return (
